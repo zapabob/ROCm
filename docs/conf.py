b1a971b432617a071ba96aafb82c163c607740d2
Saad Rahim <44449863+saadrahim@users.noreply.github.com>
2023-04-11 09:52:50 -0600

    Updating version string to include alpha(#2035)

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ shutil.copy2('../CONTRIBUTING.md','./contributing.md')
 
 from rocm_docs import ROCmDocs
 
-docs_core = ROCmDocs("ROCm Docs 5.6.0")
+docs_core = ROCmDocs("ROCm Docs 5.6.0 Alpha")
 docs_core.setup()
 docs_core.disable_main_doc_link()
 
