3e1a87a4f1472153a8bd63df0e9ba7918b540868
Mátyás Aradi <107684421+matyas-streamhpc@users.noreply.github.com>
2023-12-18 15:03:55 +0100

    Remove virtualenv build from dependencies (#2699)
    * Remove virtualenv build from dependencies
    
    * Rename ROCM_BUILD_DOCS to BUILD_DOCS

--- cmake/Modules/Dependencies.cmake
+++ cmake/Modules/Dependencies.cmake
@@ -26,7 +26,7 @@
 
 include(FetchContent)
 
-if(ROCM_BUILD_DOCS)
+if(BUILD_DOCS)
   find_package(ROCM 0.11.0 CONFIG QUIET PATHS "${ROCM_PATH}") # First version with Sphinx doc gen improvement
   if(NOT ROCM_FOUND)
     message(STATUS "ROCm CMake not found. Fetching...")
@@ -43,59 +43,5 @@ if(ROCM_BUILD_DOCS)
     find_package(ROCM CONFIG REQUIRED NO_DEFAULT_PATH PATHS "${rocm-cmake_SOURCE_DIR}")
   else()
     find_package(ROCM 0.11.0 CONFIG REQUIRED PATHS "${ROCM_PATH}")
-  endif()
-
-  if(Python_FIND_VIRTUALENV STREQUAL "ONLY" AND NOT DEFINED ENV{VIRTUAL_ENV})
-    if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/.venv")
-      message(STATUS "Python virtualenv use requested but not found. Fetching...")
-      find_program(BOOTSTRAP_PYTHON_EXE python3 REQUIRED)
-      execute_process(
-        COMMAND "${BOOTSTRAP_PYTHON_EXE}" -m pip install --user virtualenv
-        OUTPUT_QUIET
-        COMMAND_ERROR_IS_FATAL ANY
-      )
-      execute_process(
-        COMMAND "${BOOTSTRAP_PYTHON_EXE}" -m virtualenv "${CMAKE_CURRENT_BINARY_DIR}/.venv"
-        OUTPUT_QUIET
-        COMMAND_ERROR_IS_FATAL ANY
-      )
-    endif()
-    set(ENV{VIRTUAL_ENV} "${CMAKE_CURRENT_BINARY_DIR}/.venv")
-    
-    if(WIN32)
-      set(ENV{PATH} "${CMAKE_CURRENT_BINARY_DIR}/.venv/Scripts;$ENV{PATH}")
-    else()
-      set(ENV{PATH} "${CMAKE_CURRENT_BINARY_DIR}/.venv/bin:$ENV{PATH}")
-    endif()
-    
-    find_package(Python REQUIRED)
-
-    # TODO: shortcircuit if installed
-    execute_process(
-      COMMAND "${Python_EXECUTABLE}" -m pip install pip-tools
-      OUTPUT_QUIET
-      COMMAND_ERROR_IS_FATAL ANY
-    )
-
-    list(APPEND CMAKE_CONFIGURE_DEPENDS "${PROJECT_SOURCE_DIR}/docs/sphinx/requirements.in")
-    file(MAKE_DIRECTORY "$ENV{VIRTUAL_ENV}/usr/share/${PROJECT_NAME}")
-    if("${PROJECT_SOURCE_DIR}/docs/sphinx/requirements.in" IS_NEWER_THAN "$ENV{VIRTUAL_ENV}/usr/share/${PROJECT_NAME}/requirements.txt")
-      execute_process(
-        COMMAND "${Python_EXECUTABLE}" -m piptools compile
-          "${PROJECT_SOURCE_DIR}/docs/sphinx/requirements.in"
-          --output-file
-          "$ENV{VIRTUAL_ENV}/usr/share/${PROJECT_NAME}/requirements.txt"
-        OUTPUT_QUIET
-        ERROR_QUIET
-        COMMAND_ERROR_IS_FATAL ANY
-      )
-      execute_process(
-        COMMAND "${Python_EXECUTABLE}" -m piptools sync
-          "$ENV{VIRTUAL_ENV}/usr/share/${PROJECT_NAME}/requirements.txt"
-        OUTPUT_QUIET
-        ERROR_QUIET
-        COMMAND_ERROR_IS_FATAL ANY
-      )
-    endif()
   endif()
 endif()
